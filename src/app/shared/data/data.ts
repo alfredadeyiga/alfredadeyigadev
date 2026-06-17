60095f6797fbc58721619b6061818c313a4b88c8
alfred <alfred.adeyiga@brandonetech.com>
2026-06-17 14:22:32 +0100

    new project

--- src/app/shared/data/data.ts
+++ src/app/shared/data/data.ts
@@ -13,7 +13,7 @@ export const experiences = [
   {
     name: 'BrandOne',
     image: 'images/brandone.png',
-    timeFrame: 'Jun 2025 - Present',
+    timeFrame: 'Jun 2025 - Mar 2026',
     title: 'Frontend Developer',
     details: [
       'Worked on the company website with ReactJS.',
@@ -24,19 +24,29 @@ export const experiences = [
 
 export const projects = [
   {
+    name: 'PayIO',
+    description:
+      'A modern personal finance management application built to help users track balances, monitor transactions, manage expenses, and set financial goals in one place. The application features responsive dashboards, account management, transaction history, data visualization, and an intuitive user experience optimized for both desktop and mobile devices.',
+    image: 'images/project-payio.png',
+    link: 'https://payio-app.netlify.app/',
+    keywords: [
+      'React',
+      'JavaScript',
+      'HTML',
+      'TailwindCSS',
+      'API',
+      'Supabase',
+      'Backend',
+      'React Query',
+      'Recharts',
+    ],
+  },
+  {
     name: 'Stock Dash',
     description:
       'A web app which allows users to search for stock, add to portfolio, and track stock prices. It uses a stock market API to get stock market data.',
     image: 'images/project-stock.png',
     link: 'https://stock-dash-app.netlify.app/',
     keywords: ['React', 'JavaScript', 'HTML', 'Redux', 'API'],
-  },
-  {
-    name: 'Trending Movies',
-    description:
-      'A website that shows top trending movies, as well as the details and ratings of each movie. It also contains search functionality for movies on the site.',
-    image: 'images/project-movies.png',
-    link: 'https://trending-movies-site.netlify.app/',
-    keywords: ['JavaScript', 'HTML', 'CSS', 'API', 'React'],
   },
 ];
